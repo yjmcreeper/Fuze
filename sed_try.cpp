1985fca209f231b781907370f004e9b23d2a3540
yjmcreeper <2385294600@qq.com>
2025-07-30 14:30:41 +0800

    FirstCODEinCOMPANY

---
+++ sed_try.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <deque>
+#include <queue>
+#include <set>
+#include <map>
+#include <expected>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    set<int> first_set;
+    FILE* fp = fopen("./sed_try.cpp","r");
+    cout<<1<<endl;
+    return 0;
+}
